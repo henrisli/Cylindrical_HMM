028b232e16cca69fb850ca44250071ecc9d9351a
henrisli <henrisli@stud.ntnu.no>
2020-02-08 13:20:43 +0100

    Korrekt exact solver

--- Recursive_solver_approximated.R
+++ Recursive_solver_approximated.R
@@ -1,10 +1,8 @@
-library(prodlim)
-n_rows = 4
-n_cols = 100
+n_grid_test = 30
 ncolor_test = 3
 rho_test = 0.5
 potts_param_test <- c(rep(0, ncolor_test), rho_test)
-x_potts_test <- matrix(1, nrow = n_rows, ncol = n_cols)
+x_potts_test <- matrix(1, nrow = n_grid_test, ncol = n_grid_test)
 foo_test <- packPotts(x_potts_test, ncolor_test)
 out_test <- potts(foo_test, potts_param_test, nbatch = 10)
 image.plot(unpackPotts(out_test$final))
@@ -18,8 +16,8 @@ image.plot(x=X_cor, y = y_cor, z = matrix(values,nrow=100))
 values = apply(vals, MARGIN= 1, FUN = dabeley, param=parameters_test[3,])
 image.plot(x=X_cor, y = y_cor, z = matrix(values,nrow=100))
 
-simulated_sample_test = data.frame(x = rep(NA,n_rows*n_cols), theta = rep(NA,n_rows*n_cols))
-for(i in 1:(n_rows*n_cols)){
+simulated_sample_test = data.frame(x = rep(NA,n_grid_test*n_grid_test), theta = rep(NA,n_grid_test*n_grid_test))
+for(i in 1:(n_grid_test*n_grid_test)){
   k_i = spat_pros_test[i]
   theta_1_test = rwrappedcauchy(1, mu = circular(parameters_test[k_i,3]), rho = tanh(parameters_test[k_i,4]/2))
   theta_1_test = ifelse(as.numeric(theta_1_test)>pi, as.numeric(theta_1_test)-2*pi, as.numeric(theta_1_test))
@@ -33,8 +31,8 @@ simulated_sample_test = as.matrix(simulated_sample_test)
 
 xi_A_2 = matrix(NA,nrow = ncolor_test^2,ncol = 2)
 it = 1
-for (column in 1:ncolor_test){
-  for (row in 1:ncolor_test){
+for (row in 1:ncolor_test){
+  for (column in 1:ncolor_test){
     xi_A_2[it, ] = c(row,column)
     it = it + 1
   }
@@ -42,112 +40,91 @@ for (column in 1:ncolor_test){
 
 xi_A_3 = matrix(NA,nrow = ncolor_test^3,ncol = 3)
 it = 1
-for (depth in 1:ncolor_test){
+for (row in 1:ncolor_test){
   for (column in 1:ncolor_test){
-    for (row in 1:ncolor_test){
+    for (depth in 1:ncolor_test){
       xi_A_3[it, ] = c(row,column, depth)
       it = it + 1}
   }
 }
 
-xi_A_n = matrix(1:3, ncol = 1)
-for (i in 1:(n_rows)){
-  xi_A_n = cbind(rbind(xi_A_n, xi_A_n, xi_A_n), rep(1:3, each = 3^i))
-}
-moving_window_summation = matrix(0, nrow = ncolor_test^(n_rows), ncol = ncolor_test)
-for (j in 1:(ncolor_test^(n_rows))){
-  moving_window_summation[j,] = seq((j-1)%/%(ncolor_test^2)*ncolor_test^2*(ncolor_test-1)+(j), (j-1)%/%(ncolor_test^2)*ncolor_test^2*2+j+ncolor_test^2*(ncolor_test-1), l=ncolor_test)}
-  #print(j)}
-moving_window_summation = unique(moving_window_summation)
-seq_n_2 = 1:(ncolor_test^2)
-seq_n_3 = 1:(ncolor_test^3)
-conf_n_2 = c()
-for (i in 1:ncolor_test){
-  conf_n_2 = c(conf_n_2, rep(seq_n_2[((i-1)*ncolor_test + 1):(ncolor_test*i)], ncolor_test^(n_rows-1)))
-}
-conf_n_3 = c()
-for (i in 1:(ncolor_test^2)){
-  conf_n_3 = c(conf_n_3, rep(seq_n_3[((i-1)*ncolor_test + 1):(ncolor_test*i)], ncolor_test^(n_rows-2)))
-}
 
-likelihood_exact <- function(parameters_input, n_rows, data_sample, n_cols){
+likelihood <- function(parameters_input){
   rho_val = parameters_input[1]
   theta_val = matrix(parameters_input[2:(5*ncolor_test+1)], nrow=ncolor_test)
   
   # First initiate normality constants and forward probabilities
   norm_const = 0
   for (j in 1:ncolor_test){
-    norm_const = norm_const + (1/ncolor_test)*dabeley_reparam(theta_val[j,], as.vector(data_sample[1,]))
+    norm_const = norm_const + (1/ncolor_test)*dabeley_reparam(theta_val[j,], as.vector(simulated_sample_test[1,]))
   }
   norm_const = 1/norm_const
-  norm_const = c(norm_const, rep(NA, n_rows*n_cols-1))
+  norm_const = c(norm_const, rep(NA, n_grid_test^2-1))
   #norm_const = c(1/(dnorm(y_test[1],mu[1],1)*0.5+dnorm(y_test[1],mu[2],1)*0.5), rep(NA,8))
-  forward_prob = matrix(0,nrow=n_rows*n_cols,ncol = ncolor_test)
+  forward_prob = matrix(0,nrow=n_grid_test^2,ncol = ncolor_test)
   for (j in 1:ncolor_test){
-    forward_prob[1,j] = norm_const[1]*(1/ncolor_test)*dabeley_reparam(theta_val[j,], as.vector(data_sample[1,]))
+    forward_prob[1,j] = norm_const[1]*(1/ncolor_test)*dabeley_reparam(theta_val[j,], as.vector(simulated_sample_test[1,]))
   }
   #forward_prob[1,1] = norm_const[1]*0.5*dnorm(y_test[1],mu[1],1)
   #forward_prob[1,2] = norm_const[1]*0.5*dnorm(y_test[1],mu[2],1)
-  transition = matrix(0, nrow = n_cols*n_rows, ncol = ncolor_test^(n_rows+1))
-  moving_window = matrix(0, nrow = n_cols*n_rows, ncol = ncolor_test^(n_rows))
-  moving_window[1, 1:ncolor_test] = forward_prob[1,]
+  transition = matrix(0, nrow = n_grid_test^2, ncol = ncolor_test^3)
   potts_prob_2 = apply(xi_A_2, 1, function(i) exp(rho_val*ifelse(i[1]==i[2],1,0)))
   potts_prob_2 = potts_prob_2/sum(potts_prob_2)
-  potts_prob_3 = apply(xi_A_3, 1, function(i) exp(rho_val*(length(which(i[2:3]==i[1])))))
+  potts_prob_3 = apply(xi_A_3, 1, function(i) exp(rho_val*(length(which(i[1:2]==i[3])))))
   potts_prob_3 = potts_prob_3/sum(potts_prob_3)
   
   # Iterate to find constants and probabilities
-  for (t in 2:n_rows){
+  for (t in 2:n_grid_test){
     #norm_const[t] = 1/(dnorm(y_test[t],0,tau)*p*forward_prob[t-1,1] + (1-p)*dnorm(y_test[t],0,tau)*forward_prob[t-1,2] + dnorm(y_test[t],1,tau)*(1-p)*forward_prob[t-1,1] + dnorm(y_test[t],1,tau)*p*forward_prob[t-1,2])
-    for (j in 1:(ncolor_test^t)){
-      transition[t,j] = potts_prob_2[(j-1)%/%(ncolor_test^(t-2))+1]*moving_window[t-1,(j-1)%%(ncolor_test^(t-1))+1]*dabeley_reparam(theta_val[xi_A_n[j,t],], as.vector(data_sample[t,]))}
+    for (j in 1:(ncolor_test^2)){
+      transition[t,j] = potts_prob_2[j]*forward_prob[t-1,xi_A_2[j,1]]*dabeley_reparam(theta_val[xi_A_2[j,2],], as.vector(simulated_sample_test[t,]))}
     #transition[t,1] = potts_prob_2[1]*dnorm(y_test[t],mu[1],1)*forward_prob[t-1,1]
     #transition[t,2] = potts_prob_2[2]*dnorm(y_test[t],mu[2],1)*forward_prob[t-1,1]
     #transition[t,3] = potts_prob_2[3]*dnorm(y_test[t],mu[1],1)*forward_prob[t-1,2]
     #transition[t,4] = potts_prob_2[4]*dnorm(y_test[t],mu[2],1)*forward_prob[t-1,2]
     norm_const[t] = 1/sum(transition[t,])
     transition[t,] = transition[t,]*norm_const[t]
-    #for (j in 1:(ncolor_test^(t-1))){
-    #  moving_window[t,j] = sum(transition[t, which(apply(xi_A_n[1:(ncolor_test^t),1:(t-1)], 1, function(x) identical(x, xi_A_n[j,1:(t-1)])))])
-    #}
-    moving_window[t,1:ncolor_test^t] = transition[t,1:ncolor_test^t]
     for (j in 1:ncolor_test){
-      forward_prob[t,j] = sum(transition[t,which(xi_A_n[,t]==j)])
+      forward_prob[t,j] = sum(transition[t,which(xi_A_2[,2]==j)])
     }
     #forward_prob[t,1] = transition[t,1] + transition[t,3]
     #forward_prob[t,2] = transition[t,2] + transition[t,4]
   }
-  for (t in (n_rows+1):(n_rows*n_cols)){
+  for (t in (n_grid_test+1):(n_grid_test^2)){
     if ((t-1)%%n_grid_test==0){
-      for (j in 1:(ncolor_test^(n_rows+1))){
-        transition[t,j] = potts_prob_2[conf_n_2[j]]*dabeley_reparam(theta_val[xi_A_n[j,n_rows+1],], as.vector(data_sample[t,]))*moving_window[t-1,(j-1)%%(ncolor_test^(n_rows))+1]
-      }
-      # rekkefølge: t-n, t-(n-1), ... , t-1, t for telle oppover i potts_prob og utsummering for moving window
+      for (j in 1:(ncolor_test^2)){
+        transition[t,j] = potts_prob_2[j]*dabeley_reparam(theta_val[xi_A_2[j,2],], as.vector(simulated_sample_test[t,]))*forward_prob[t-n_grid_test,xi_A_2[j,1]]}
+      #transition[t,1] = potts_prob_2[1]*dnorm(y_test[t],mu[1],1)*forward_prob[t-1,1]
+      #transition[t,2] = potts_prob_2[2]*dnorm(y_test[t],mu[2],1)*forward_prob[t-1,1]
+      #transition[t,3] = potts_prob_2[3]*dnorm(y_test[t],mu[1],1)*forward_prob[t-1,2]
+      #transition[t,4] = potts_prob_2[4]*dnorm(y_test[t],mu[2],1)*forward_prob[t-1,2]
       norm_const[t] = 1/sum(transition[t,])
       transition[t,] = transition[t,]*norm_const[t]
-      for (j in 1:(ncolor_test^n_rows)){
-        moving_window[t,j] = sum(transition[t, (j-1)*ncolor_test+1:ncolor_test])
-      }
       for (j in 1:ncolor_test){
-        forward_prob[t,j] = sum(transition[t,which(xi_A_n[,n_rows+1]==j)])
+        forward_prob[t,j] = sum(transition[t,which(xi_A_2[,2]==j)])
       }
+      #forward_prob[t,1] = transition[t,1] + transition[t,3]
+      #forward_prob[t,2] = transition[t,2] + transition[t,4]
     }else{
-      for (j in 1:(ncolor_test^(n_rows+1))){
-        transition[t,j] = potts_prob_3[conf_n_3[j]]*dabeley_reparam(theta_val[xi_A_n[j,n_rows+1],], as.vector(data_sample[t,]))*moving_window[t-1,(j-1)%%(ncolor_test^(n_rows))+1]
+      for (j in 1:(ncolor_test^3)){
+        transition[t,j] = potts_prob_3[j]*dabeley_reparam(theta_val[xi_A_3[j,3],], as.vector(simulated_sample_test[t,]))*forward_prob[t-1,xi_A_3[j,2]]*forward_prob[t-n_grid_test, xi_A_3[j,1]]
       }
-     
+      #transition[t,1] = potts_prob_3[1]*dnorm(y_test[t],mu[1],1)*forward_prob[t-1,1]*forward_prob[t-n_grid_test, 1]
+      #transition[t,2] = potts_prob_3[2]*dnorm(y_test[t],mu[2],1)*forward_prob[t-1,1]*forward_prob[t-n_grid_test, 1]
+      #transition[t,3] = potts_prob_3[3]*dnorm(y_test[t],mu[1],1)*forward_prob[t-1,2]*forward_prob[t-n_grid_test, 1]
+      #transition[t,4] = potts_prob_3[4]*dnorm(y_test[t],mu[2],1)*forward_prob[t-1,2]*forward_prob[t-n_grid_test, 1]
+      #transition[t,5] = potts_prob_3[5]*dnorm(y_test[t],mu[1],1)*forward_prob[t-1,1]*forward_prob[t-n_grid_test, 2]
+      #transition[t,6] = potts_prob_3[6]*dnorm(y_test[t],mu[2],1)*forward_prob[t-1,1]*forward_prob[t-n_grid_test, 2]
+      #transition[t,7] = potts_prob_3[7]*dnorm(y_test[t],mu[1],1)*forward_prob[t-1,2]*forward_prob[t-n_grid_test, 2]
+      #transition[t,8] = potts_prob_3[8]*dnorm(y_test[t],mu[2],1)*forward_prob[t-1,2]*forward_prob[t-n_grid_test, 2]
       norm_const[t] = 1/sum(transition[t,])
       transition[t,] = transition[t,]*norm_const[t]
-      for (j in 1:(ncolor_test^n_rows)){
-        moving_window[t,j] = sum(transition[t, (j-1)*ncolor_test+1:ncolor_test])
-      }
       for (j in 1:ncolor_test){
-        forward_prob[t,j] = sum(transition[t,which(xi_A_n[,n_rows+1]==j)])
+        forward_prob[t,j] = sum(transition[t,which(xi_A_3[,3]==j)])
       }
       #forward_prob[t,1] = transition[t,1] + transition[t,3] + transition[t,5] + transition[t,7]
       #forward_prob[t,2] = transition[t,2] + transition[t,4] + transition[t,6] + transition[t,8]
     }
-    #print(t)
   }
   #print(norm_const)
   #print(forward_prob)
@@ -156,8 +133,8 @@ likelihood_exact <- function(parameters_input, n_rows, data_sample, n_cols){
   #return(norm_const)
 }
 
-neg_likelihood_exact <- function(parameters_input, n_rows, data_sample, n_cols){
-  return(-likelihood_exact(parameters_input, n_rows, data_sample, n_cols))
+neg_likelihood <- function(parameters){
+  return(-likelihood(parameters))
 }
 
 
@@ -176,11 +153,10 @@ init_param = c(runif(1,0,log(1+sqrt(ncolor_test))), runif(ncolor_test*5, paramet
 #init_param[c(2,3,4,5,8,9)] = log(init_param[c(2,3,4,5,8,9)])
 #init_param[c(6,7)] = tan(init_param[c(6,7)]/2)
 #init_param[c(10,11)] = atanh(init_param[c(10,11)])
-likelihood_exact(init_param, n_rows = n_rows, data_sample = simulated_sample_test, n_cols = n_cols)
-neg_likelihood_exact(init_param, n_rows = n_rows, data_sample = simulated_sample_test, n_cols = n_cols)
+likelihood(init_param)
 
 time_test = Sys.time()
-optimal = optim(init_param, neg_likelihood_exact, method = "BFGS", control = list(trace=6, REPORT = 1), n_rows = n_rows, data_sample = simulated_sample_test, n_cols = n_cols)
+optimal = optim(init_param, neg_likelihood, method = "BFGS", control = list(trace=6, REPORT = 1))
 #likelihood(c(0.5,-5,5,15,25, 35))
 Sys.time() - time_test
 optimal$par
@@ -205,9 +181,3 @@ values = apply(vals, MARGIN= 1, FUN = dabeley, param=estimated_param[2,])
 image.plot(x=X_cor, y = y_cor, z = matrix(values,nrow=100))
 values = apply(vals, MARGIN= 1, FUN = dabeley, param=estimated_param[3,])
 image.plot(x=X_cor, y = y_cor, z = matrix(values,nrow=100))
-
-
-ttime = Sys.time()
-likelihood_exact(parameters_test_reparam, n_rows, data_sample, n_cols)
-#likelihood(parameters_test_reparam)
-Sys.time() - ttime
